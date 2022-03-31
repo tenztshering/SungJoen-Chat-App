29ae86e4065e65430881be9b779d6ace5f935154
Tenzin Tshering <89956683+tenztshering@users.noreply.github.com>
2022-03-30 23:09:11 -1100

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
   return (
     <div id="login-page">
         <div id="login-card">
-            <h2>Welcome to Unichat!</h2>
+            <h2>Welcome to SungJoen App!</h2>
             <div className="login-button google"
             onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
             >
